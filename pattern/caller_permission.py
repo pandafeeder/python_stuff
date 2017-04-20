0d306eb9584118f393d1ec739b03d50ad8948ac4
PandaFeeder <pandafeeder@gmail.com>
2017-04-20 23:33:38 +0800

    log some design patter

---
+++ pattern/caller_permission.py
@@ -0,0 +1,21 @@
+import sys
+
+class ViolateCallingProtocolException(Exception): pass
+
+def callee():
+    caller_frame = sys._getframe(1)
+    if not caller_frame.f_code.co_name.endswith("_protocol"):
+        raise ViolateCallingProtocolException("caller function name must ends with _protocol")
+    print("Do the job")
+
+
+def comply_protocol():
+    """this will work as normal"""
+    callee()
+
+def violate_protocol_func():
+    """this will throw ViolateCallingProtocolException"""
+    callee()
+
+comply_protocol()
+violate_protocol_func()
